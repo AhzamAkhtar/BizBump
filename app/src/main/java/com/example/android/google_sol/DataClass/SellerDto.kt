ada324ec9ebb56dec6a3e4c3a004b9a547183834
Ahzam Akhtar <ahzam8186@gmail.com>
2023-01-30 22:44:10 +0530

    First Phase Done

--- app/src/main/java/com/example/android/google_sol/DataClass/SellerDto.kt
+++ app/src/main/java/com/example/android/google_sol/DataClass/SellerDto.kt
@@ -1,14 +1,8 @@
 package com.example.android.google_sol.DataClass
 
-import com.google.gson.annotations.SerializedName
-
 data class SellerDto(
-    @SerializedName("Name")
-    val name : String,
-    @SerializedName("Type")
+    val Name : String,
     val Type : String,
-    @SerializedName("Lat")
-    val Lat : Int,
-    @SerializedName("Lng")
-    val Lng : Int
+    val Lat : String,
+    val Lng : String
 )
